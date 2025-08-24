6fbb9bb5e90ebff114c50cbf063e0d3eef90d877
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-08-24 18:10:16 -0400

    fixed formatting on table

--- frontend/app/components/MacronutrientTable.tsx
+++ frontend/app/components/MacronutrientTable.tsx
@@ -19,19 +19,22 @@ export default function MacronutrientTable({
 }: MacroNutrientTableProps) {
   return (
     <div>
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <table>
         <thead>
           <tr>
-            <th>name</th>
-            <th>calories</th>
-            <th>{macronutrient}</th>
+            <th>Name</th>
+            <th>Calories</th>
+            <th>
+              {macronutrient.charAt(0).toUpperCase() + macronutrient.slice(1)}
+            </th>
           </tr>
         </thead>
         <tbody>
           {data.map((item) => (
             <tr key={item.dish}>
               <td>{item.dish}</td>
+              <td>{item.calories}</td>
               <td>{item[macronutrient]}</td>
             </tr>
           ))}
